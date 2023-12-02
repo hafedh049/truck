a8b190e361a61673e199c184287c16cccf5a8a5a
Hafedh Gunichi <120606876+hafedh049@users.noreply.github.com>
2023-12-02 22:09:51 +0100

    Modified  lib/home.dart

--- lib/home.dart
+++ lib/home.dart
@@ -10,6 +10,10 @@ class Home extends StatefulWidget {
 class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return const Scaffold(
+      body: Column(
+        children: <Widget>[],
+      ),
+    );
   }
 }
