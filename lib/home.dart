eb15fcd3a63c1f47f126dbb2943e6bf25f9888cb
Hafedh Gunichi <120606876+hafedh049@users.noreply.github.com>
2023-12-02 22:10:31 +0100

    Modified  lib/home.dart

--- lib/home.dart
+++ lib/home.dart
@@ -12,6 +12,7 @@ class _HomeState extends State<Home> {
   Widget build(BuildContext context) {
     return const Scaffold(
       body: Column(
+        mainAxisSize: MainAxisSize.min,
         children: <Widget>[],
       ),
     );
