53b15e7f7d535c9cef5f19a1d3e482ad09340596
Hafedh Gunichi <120606876+hafedh049@users.noreply.github.com>
2023-12-03 22:04:22 +0100

    Modified  lib/home.dart

--- lib/home.dart
+++ lib/home.dart
@@ -20,7 +20,7 @@ class _HomeState extends State<Home> {
 
   @override
   void initState() {
-    _tts.first["callback"] = () async {};
+    _items.first["callback"] = () async {};
     super.initState();
   }
 
