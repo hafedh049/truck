946e8c0d8ca3ab5753a8f28b1718b4ad2440d0f1
Hafedh Gunichi <120606876+hafedh049@users.noreply.github.com>
2023-12-03 22:48:26 +0100

    Modified  lib/wait.dart

---
+++ lib/wait.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class Wait extends StatelessWidget {
+  const Wait({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
