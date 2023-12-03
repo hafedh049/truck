7f8f6fdfd1da66ae7e27c6f074ef276135732d6d
Hafedh Gunichi <120606876+hafedh049@users.noreply.github.com>
2023-12-03 22:04:00 +0100

    Modified  lib/home.dart

--- lib/home.dart
+++ lib/home.dart
@@ -16,9 +16,7 @@ class _HomeState extends State<Home> {
     <String, dynamic>{
       "title": "Repeat Last Message",
       "icon": Bootstrap.arrow_repeat,
-      "callback": () async {
-        await _tts.speak("Hi How Are You ?.");
-      },
+      "callback": () async {},
     },
     <String, dynamic>{"title": "Message Understood", "icon": Bootstrap.check2_circle, "callback": () {}},
     <String, dynamic>{"title": "I Have A Problem", "icon": FontAwesome.circle_exclamation, "callback": () {}},
