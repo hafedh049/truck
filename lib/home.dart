208a9eb03e8cac4d151e8f564282167ab8750a85
Hafedh Gunichi <120606876+hafedh049@users.noreply.github.com>
2023-12-03 22:08:35 +0100

    Modified  lib/home.dart

--- lib/home.dart
+++ lib/home.dart
@@ -24,7 +24,7 @@ class _HomeState extends State<Home> {
       await _tts.speak("Hi my name is Hafedh!");
     };
     _items[1]["callback"] = () async {};
-    _items[2]["callback"] = () async {Navigator.push(context,MaterialPageRoute(builder: (context) => ) )};
+    _items[2]["callback"] = () async {Navigator.push(context,MaterialPageRoute(builder: (context) => const ChatRoom()) )};
     super.initState();
   }
 
