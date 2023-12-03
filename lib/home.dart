91c6c86b7b99825f2d99705e955dc262c772d042
Hafedh Gunichi <120606876+hafedh049@users.noreply.github.com>
2023-12-03 22:01:17 +0100

    Modified  lib/home.dart

--- lib/home.dart
+++ lib/home.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:icons_plus/icons_plus.dart';
+import 'package:text_to_speech/text_to_speech.dart';
 import 'package:truck/utils/globals.dart';
 
 class Home extends StatefulWidget {
@@ -14,7 +15,9 @@ class _HomeState extends State<Home> {
     <String, dynamic>{
       "title": "Repeat Last Message",
       "icon": Bootstrap.arrow_repeat,
-      "callback": () {},
+      "callback": () {
+        TextToSpeech();
+      },
     },
     <String, dynamic>{"title": "Message Understood", "icon": Bootstrap.check2_circle, "callback": () {}},
     <String, dynamic>{"title": "I Have A Problem", "icon": FontAwesome.circle_exclamation, "callback": () {}},
