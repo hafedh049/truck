265204d860ad443de54a460208cf87b97d50da25
Hafedh Gunichi <120606876+hafedh049@users.noreply.github.com>
2023-12-04 20:57:48 +0100

    Modified  lib/wrong.dart

--- lib/wrong.dart
+++ lib/wrong.dart
@@ -7,12 +7,14 @@ class Wrong extends StatelessWidget {
   final String errorMessage;
   @override
   Widget build(BuildContext context) => Scaffold(
-        body: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            LottieBuilder.asset("assets/lotties/wrong.json", width: MediaQuery.sizeOf(context).width * .8),
-            Flexible(child: Text(errorMessage, style: TextStyle(color: blue, fontSize: 16, fontWeight: FontWeight.w400))),
-          ],
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              LottieBuilder.asset("assets/lotties/wrong.json", width: MediaQuery.sizeOf(context).width * .8),
+              Flexible(child: Text(errorMessage, style: TextStyle(color: blue, fontSize: 16, fontWeight: FontWeight.w400))),
+            ],
+          ),
         ),
       );
 }
