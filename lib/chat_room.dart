e26fe5db694ba281cfff2d5648f61e49ec24345d
Hafedh Gunichi <120606876+hafedh049@users.noreply.github.com>
2023-12-04 22:02:26 +0100

    Modified  lib/chat_room.dart

--- lib/chat_room.dart
+++ lib/chat_room.dart
@@ -66,10 +66,11 @@ class _ChatRoomState extends State<ChatRoom> {
 
                 data["reaction"]['reactions'] = data["reaction"]['reactions'].cast<String>();
                 data["reaction"]['reactedUserIds'] = data["reaction"]['reactedUserIds'].cast<String>();
-                print(data);
+
                 return Message.fromJson(data);
               },
             ).toList();
+            print(data);
             _noMessagesYet = 0;
           }
           return ChatView(
