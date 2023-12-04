973fb0826ebedd15a56e40e9c59344a64063e5f9
Hafedh Gunichi <120606876+hafedh049@users.noreply.github.com>
2023-12-04 21:04:58 +0100

    Modified  lib/chat_room.dart

--- lib/chat_room.dart
+++ lib/chat_room.dart
@@ -33,7 +33,7 @@ class _ChatRoomState extends State<ChatRoom> {
   void initState() {
     _timer = Timer.periodic(1.seconds, (Timer timer) => _noMessagesYet == 60 ? Navigator.pop(context) : _noMessagesYet += 1);
     _currentUser = ChatUser(id: '1', name: 'Flutter', profilePhoto: _profileImage);
-    ;
+
     super.initState();
   }
 
