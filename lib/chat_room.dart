2f6a8540b55ed1aabb26757679b09bab69c00db5
Hafedh Gunichi <120606876+hafedh049@users.noreply.github.com>
2023-12-04 23:25:57 +0100

    Modified  lib/chat_room.dart

--- lib/chat_room.dart
+++ lib/chat_room.dart
@@ -158,7 +158,6 @@ class _ChatRoomState extends State<ChatRoom> {
   }
 
   void _onSendTap(String message, ReplyMessage replyMessage, MessageType messageType) async {
-    print(messageType);
     final String id = const Uuid().v8();
     await FirebaseFirestore.instance.collection("messages").add(
       <String, dynamic>{
