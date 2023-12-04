a9fcfd2a062f019697dc91f1055cd8b0becb4f67
Hafedh Gunichi <120606876+hafedh049@users.noreply.github.com>
2023-12-04 23:54:18 +0100

    Modified  lib/chat_room.dart

--- lib/chat_room.dart
+++ lib/chat_room.dart
@@ -168,7 +168,9 @@ class _ChatRoomState extends State<ChatRoom> {
 
     if (messageType == MessageType.image) {
       await FirebaseStorage.instance.ref("images/").child(id).putFile(File(message)).then((TaskSnapshot tasksnapshot) async => message = await tasksnapshot.ref.getDownloadURL());
-    } else if (messageType == MessageType.voice) {}
+    } else if (messageType == MessageType.voice) {
+      await FirebaseStorage.instance.ref("images/").child(id).putFile(File(message)).then((TaskSnapshot tasksnapshot) async => message = await tasksnapshot.ref.getDownloadURL());
+    }
 
     await FirebaseFirestore.instance.collection("messages").add(
       <String, dynamic>{
