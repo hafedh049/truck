60968b565e3ab9ce6f3221185460581c83a70ad1
Hafedh Gunichi <120606876+hafedh049@users.noreply.github.com>
2023-12-04 23:43:31 +0100

    Modified  lib/chat_room.dart

--- lib/chat_room.dart
+++ lib/chat_room.dart
@@ -73,11 +73,15 @@ class _ChatRoomState extends State<ChatRoom> {
               }
               _noMessagesYet = 0;
             }
-            return ChatView(isLastPage: false,
+            return ChatView(
+isLastPage: false,
   featureActiveConfig: FeatureActiveConfig(
     enablePagination: true,
   ),
   loadMoreData: chatController.loadMoreData
+
+
+
               currentUser: _currentUser,
               chatController: _chatController,
               onSendTap: _onSendTap,
