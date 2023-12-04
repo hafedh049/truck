8c9d79f1c24d3bad65902eacce511864df47c51e
Hafedh Gunichi <120606876+hafedh049@users.noreply.github.com>
2023-12-04 19:47:55 +0100

    Modified  lib/utils/data.dart

--- lib/utils/data.dart
+++ lib/utils/data.dart
@@ -1,9 +1,11 @@
 import 'package:chatview/chatview.dart';
+import 'package:flutter_animate/flutter_animate.dart';
 
 class Data {
   static const profileImage = "https://raw.githubusercontent.com/SimformSolutionsPvtLtd/flutter_showcaseview/master/example/assets/simform.png";
   static final messageList = [
-    Message(messageType: MessageType.text,reaction:Reaction(reactions: [], reactedUserIds: []) ,replyMessage:ReplyMessage() ,voiceMessageDuration: ,
+    Message(
+      messageType: MessageType.text, reaction: Reaction(reactions: [], reactedUserIds: []), replyMessage: ReplyMessage(), voiceMessageDuration: 1.seconds,
       id: '1',
       message: "Hi!",
       createdAt: DateTime.now(),
