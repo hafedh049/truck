5254bbd6aaff2f2ac71d2af26b397b4952bba905
Hafedh Gunichi <120606876+hafedh049@users.noreply.github.com>
2023-12-04 22:48:21 +0100

    Modified  lib/chat_room.dart

--- lib/chat_room.dart
+++ lib/chat_room.dart
@@ -70,7 +70,7 @@ class _ChatRoomState extends State<ChatRoom> {
                 return Message.fromJson(data);
               },
             ).toList();
-            //  _noMessagesYet = 0;
+            _noMessagesYet = 0;
           }
           return ChatView(
             currentUser: _currentUser,
