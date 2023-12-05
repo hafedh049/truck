ac95b544a1d01826318318e49c6a287888f0bb82
Hafedh Gunichi <120606876+hafedh049@users.noreply.github.com>
2023-12-05 22:04:07 +0100

    Modified  lib/utils/globals.dart

--- lib/utils/globals.dart
+++ lib/utils/globals.dart
@@ -4,3 +4,5 @@ import 'package:flutter/material.dart';
 const Color transparent = Colors.transparent;
 Color blue = Colors.blue.withOpacity(.8);
 const Color gray = Color.fromARGB(255, 51, 56, 66);
+
+final String documentsPath = "";
