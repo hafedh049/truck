a648b019abc82354f45d7fa0696ffc744cccc3b5
Hafedh Gunichi <120606876+hafedh049@users.noreply.github.com>
2023-12-05 22:04:20 +0100

    Modified  lib/utils/globals.dart

--- lib/utils/globals.dart
+++ lib/utils/globals.dart
@@ -5,4 +5,4 @@ const Color transparent = Colors.transparent;
 Color blue = Colors.blue.withOpacity(.8);
 const Color gray = Color.fromARGB(255, 51, 56, 66);
 
-const String documentsPath = "";
+String documentsPath = "";
