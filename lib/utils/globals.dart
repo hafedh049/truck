ee9da18d8169bdca2d77be151c7483e62db89a19
Hafedh Gunichi <120606876+hafedh049@users.noreply.github.com>
2023-12-08 23:40:25 +0100

    Modified  lib/utils/globals.dart

--- lib/utils/globals.dart
+++ lib/utils/globals.dart
@@ -4,7 +4,7 @@ import 'package:hive/hive.dart';
 import 'package:truck/models/user_model.dart';
 
 const Color transparent = Colors.transparent;
-const Color white = Colors.white;
+const Color white = Color.fromARGB(207, 255, 255, 255);
 Color blue = Colors.blue.withOpacity(.8);
 const Color gray = Color.fromARGB(255, 51, 56, 66);
 
