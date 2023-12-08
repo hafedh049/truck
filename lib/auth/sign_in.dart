fff79a57e955a168b3aa44266df09875ef08f733
Hafedh Gunichi <120606876+hafedh049@users.noreply.github.com>
2023-12-09 00:17:30 +0100

    Modified  lib/auth/sign_in.dart

--- lib/auth/sign_in.dart
+++ lib/auth/sign_in.dart
@@ -30,111 +30,107 @@ class _SignInState extends State<SignIn> {
       resizeToAvoidBottomInset: false,
       body: GestureDetector(
         onTap: () => FocusScope.of(context).unfocus(),
-        child: Stack(
+        child: Column(
           children: <Widget>[
-            Column(
-              children: <Widget>[
-                SizedBox(height: MediaQuery.sizeOf(context).height * .4),
-                Expanded(
-                  child: Container(
-                    decoration: const BoxDecoration(color: gray, borderRadius: BorderRadius.only(topLeft: Radius.circular(25), topRight: Radius.circular(25))),
-                    padding: const EdgeInsets.all(24),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
+            SizedBox(height: MediaQuery.sizeOf(context).height * .4),
+            Expanded(
+              child: Container(
+                decoration: const BoxDecoration(color: gray, borderRadius: BorderRadius.only(topLeft: Radius.circular(25), topRight: Radius.circular(25))),
+                padding: const EdgeInsets.all(24),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Row(
                       children: <Widget>[
-                        Row(
-                          children: <Widget>[
-                            Text(userLocalSettings!.get("first_time") ? "Welcome" : "Welcome\nBack", style: const TextStyle(fontSize: 28, color: white, fontWeight: FontWeight.w500)),
-                            const Spacer(),
-                            Container(
-                              decoration: BoxDecoration(color: white.withOpacity(.2), shape: BoxShape.circle),
-                              padding: const EdgeInsets.all(16),
-                              child: Icon(FontAwesome.hands, color: yellow, size: 25),
-                            ),
-                          ],
+                        Text(userLocalSettings!.get("first_time") ? "Welcome" : "Welcome\nBack", style: const TextStyle(fontSize: 28, color: white, fontWeight: FontWeight.w500)),
+                        const Spacer(),
+                        Container(
+                          decoration: BoxDecoration(color: white.withOpacity(.2), shape: BoxShape.circle),
+                          padding: const EdgeInsets.all(16),
+                          child: const Icon(FontAwesome.hands, color: yellow, size: 25),
                         ),
-                        const SizedBox(height: 20),
-                        Form(
-                          child: Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: <Widget>[
-                              TextFormField(
-                                controller: _emailController,
+                      ],
+                    ),
+                    const SizedBox(height: 20),
+                    Form(
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: <Widget>[
+                          TextFormField(
+                            controller: _emailController,
+                            style: const TextStyle(color: white, fontSize: 16, fontWeight: FontWeight.w400),
+                            decoration: InputDecoration(
+                              border: OutlineInputBorder(borderRadius: BorderRadius.circular(12), borderSide: BorderSide(width: .6, color: gray.withOpacity(.1))),
+                              focusedBorder: OutlineInputBorder(borderRadius: BorderRadius.circular(12), borderSide: BorderSide(width: .8, color: white.withOpacity(.6))),
+                              hintText: "Email",
+                              contentPadding: const EdgeInsets.all(24),
+                              hintStyle: TextStyle(color: white.withOpacity(.5), fontSize: 16, fontWeight: FontWeight.w400),
+                            ),
+                          ),
+                          const SizedBox(height: 20),
+                          StatefulBuilder(
+                            builder: (BuildContext context, void Function(void Function()) _) {
+                              return TextFormField(
+                                controller: _passwordController,
+                                obscureText: !_passwordState,
                                 style: const TextStyle(color: white, fontSize: 16, fontWeight: FontWeight.w400),
                                 decoration: InputDecoration(
                                   border: OutlineInputBorder(borderRadius: BorderRadius.circular(12), borderSide: BorderSide(width: .6, color: gray.withOpacity(.1))),
                                   focusedBorder: OutlineInputBorder(borderRadius: BorderRadius.circular(12), borderSide: BorderSide(width: .8, color: white.withOpacity(.6))),
-                                  hintText: "Email",
+                                  hintText: "Password",
+                                  suffixIcon: IconButton(onPressed: () => _(() => _passwordState = !_passwordState), icon: Icon(!_passwordState ? Icons.visibility_off : Icons.visibility)),
                                   contentPadding: const EdgeInsets.all(24),
                                   hintStyle: TextStyle(color: white.withOpacity(.5), fontSize: 16, fontWeight: FontWeight.w400),
                                 ),
-                              ),
-                              const SizedBox(height: 20),
-                              StatefulBuilder(
-                                builder: (BuildContext context, void Function(void Function()) _) {
-                                  return TextFormField(
-                                    controller: _passwordController,
-                                    obscureText: !_passwordState,
-                                    style: const TextStyle(color: white, fontSize: 16, fontWeight: FontWeight.w400),
-                                    decoration: InputDecoration(
-                                      border: OutlineInputBorder(borderRadius: BorderRadius.circular(12), borderSide: BorderSide(width: .6, color: gray.withOpacity(.1))),
-                                      focusedBorder: OutlineInputBorder(borderRadius: BorderRadius.circular(12), borderSide: BorderSide(width: .8, color: white.withOpacity(.6))),
-                                      hintText: "Password",
-                                      suffixIcon: IconButton(onPressed: () => _(() => _passwordState = !_passwordState), icon: Icon(!_passwordState ? Icons.visibility_off : Icons.visibility)),
-                                      contentPadding: const EdgeInsets.all(24),
-                                      hintStyle: TextStyle(color: white.withOpacity(.5), fontSize: 16, fontWeight: FontWeight.w400),
-                                    ),
-                                  );
-                                },
-                              ),
-                              const SizedBox(height: 20),
-                              Row(
-                                children: <Widget>[
-                                  const Spacer(),
-                                  InkWell(
-                                    highlightColor: transparent,
-                                    splashColor: transparent,
-                                    hoverColor: transparent,
-                                    onTap: () {},
-                                    child: Text("Forget Password?", style: TextStyle(color: white.withOpacity(.8), fontSize: 16, fontWeight: FontWeight.w400)),
-                                  ),
-                                ],
-                              ),
-                            ],
+                              );
+                            },
                           ),
-                        ),
-                        const SizedBox(height: 40),
-                        InkWell(
-                          onTap: () async {
-                            await Get.to(const Home());
-                          },
-                          child: Container(
-                            decoration: BoxDecoration(color: teal, borderRadius: BorderRadius.circular(12)),
-                            width: MediaQuery.sizeOf(context).width,
-                            padding: const EdgeInsets.all(20),
-                            child: const Center(child: Text("Sign in", style: TextStyle(fontSize: 18, fontWeight: FontWeight.w500))),
-                          ),
-                        ),
-                        const SizedBox(height: 20),
-                        InkWell(
-                          highlightColor: transparent,
-                          splashColor: transparent,
-                          hoverColor: transparent,
-                          onTap: () async => Get.to(const SignUp()),
-                          child: Row(
-                            mainAxisAlignment: MainAxisAlignment.center,
+                          const SizedBox(height: 20),
+                          Row(
                             children: <Widget>[
-                              Text("Don't have an account?", style: TextStyle(color: white.withOpacity(.5), fontSize: 16, fontWeight: FontWeight.w400)),
-                              const SizedBox(width: 5),
-                              const Text("Get Started", style: TextStyle(color: teal, fontSize: 16, fontWeight: FontWeight.w400)),
+                              const Spacer(),
+                              InkWell(
+                                highlightColor: transparent,
+                                splashColor: transparent,
+                                hoverColor: transparent,
+                                onTap: () {},
+                                child: Text("Forget Password?", style: TextStyle(color: white.withOpacity(.8), fontSize: 16, fontWeight: FontWeight.w400)),
+                              ),
                             ],
                           ),
-                        ),
-                      ],
+                        ],
+                      ),
                     ),
-                  ),
+                    const SizedBox(height: 40),
+                    InkWell(
+                      onTap: () async {
+                        await Get.to(const Home());
+                      },
+                      child: Container(
+                        decoration: BoxDecoration(color: teal, borderRadius: BorderRadius.circular(12)),
+                        width: MediaQuery.sizeOf(context).width,
+                        padding: const EdgeInsets.all(20),
+                        child: const Center(child: Text("Sign in", style: TextStyle(fontSize: 18, fontWeight: FontWeight.w500))),
+                      ),
+                    ),
+                    const SizedBox(height: 20),
+                    InkWell(
+                      highlightColor: transparent,
+                      splashColor: transparent,
+                      hoverColor: transparent,
+                      onTap: () async => Get.to(const SignUp()),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: <Widget>[
+                          Text("Don't have an account?", style: TextStyle(color: white.withOpacity(.5), fontSize: 16, fontWeight: FontWeight.w400)),
+                          const SizedBox(width: 5),
+                          const Text("Get Started", style: TextStyle(color: teal, fontSize: 16, fontWeight: FontWeight.w400)),
+                        ],
+                      ),
+                    ),
+                  ],
                 ),
-              ],
+              ),
             ),
           ],
         ),
