daf7ff9e61b488c07ad5704a64cf76f1b4c875cf
Hafedh Gunichi <120606876+hafedh049@users.noreply.github.com>
2023-12-08 23:35:50 +0100

    Modified  lib/models/user_model.dart

--- lib/models/user_model.dart
+++ lib/models/user_model.dart
@@ -3,12 +3,10 @@ import 'package:flutter/material.dart';
 @immutable
 class UserModel {
   final String uid;
-  final String password;
   final String email;
   final String username;
-  final String type;
 
-  const UserModel({required this.type, required this.uid, required this.password, required this.email, required this.username});
+  const UserModel({required this.uid, required this.email, required this.username});
 
   factory UserModel.fromJson(Map<String, dynamic> json) {
     return UserModel(type: json["type"] as String, uid: json['uid'] as String, password: json['password'] as String, email: json['email'] as String, username: json['username'] as String);
