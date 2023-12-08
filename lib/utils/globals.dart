81f219dbd722a3c251400295895d641f7f7bc437
Hafedh Gunichi <120606876+hafedh049@users.noreply.github.com>
2023-12-08 23:41:30 +0100

    Modified  lib/utils/globals.dart

--- lib/utils/globals.dart
+++ lib/utils/globals.dart
@@ -5,10 +5,7 @@ import 'package:truck/models/user_model.dart';
 
 const Color transparent = Colors.transparent;
 const Color white = Color.fromARGB(207, 255, 255, 255);
-Color blue = Colors.blue.withOpacity(.8);
-const Color transparent = Colors.transparent;
 const Color gray = Color.fromARGB(255, 51, 56, 66);
-const Color white = Colors.white;
 const Color yellow = Colors.yellow;
 const Color blue = Colors.blueAccent;
 const Color teal = Colors.teal;
