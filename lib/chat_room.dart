cd97bab1b36a2ca711cd8e82f723ee462023af07
Hafedh Gunichi <120606876+hafedh049@users.noreply.github.com>
2023-12-08 22:43:37 +0100

    Modified  lib/chat_room.dart

--- lib/chat_room.dart
+++ lib/chat_room.dart
@@ -31,7 +31,7 @@ class _ChatRoomState extends State<ChatRoom> {
   @override
   void initState() {
     _timer = Timer.periodic(1.seconds, (Timer timer) => _noMessagesYet == 60 ? Navigator.pop(context) : _noMessagesYet += 1);
-    _currentUser = ChatUser(id: '1', name: 'Flutter', profilePhoto: _profileImage);
+    _currentUser = ChatUser(id: 'me', name: 'Truck-***', profilePhoto: _profileImage);
     _chatController = ChatController(initialMessageList: <Message>[], scrollController: ScrollController(), chatUsers: <ChatUser>[ChatUser(id: '0', name: 'Discord', profilePhoto: _profileImage)]);
     super.initState();
   }
