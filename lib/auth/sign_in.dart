b9bfef97de0481686b21744812e7be4e872b990e
Hafedh Gunichi <120606876+hafedh049@users.noreply.github.com>
2023-12-08 23:45:16 +0100

    Modified  lib/auth/sign_in.dart M  lib/auth/sign_up.dart

--- lib/auth/sign_in.dart
+++ lib/auth/sign_in.dart
@@ -1,14 +1,10 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:firebase_auth/firebase_auth.dart';
+import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_animate/flutter_animate.dart';
-
+import 'package:footy_shorts/home.dart';
+import 'package:footy_shorts/utils/globals.dart';
+import 'package:footy_shorts/views/auth/sign_up.dart';
+import 'package:get/get.dart';
 import 'package:icons_plus/icons_plus.dart';
-import 'package:truck/auth/sign_up.dart';
-import 'package:truck/home.dart';
-import 'package:truck/models/user_model.dart';
-import 'package:truck/utils/globals.dart';
-import 'package:truck/utils/methods.dart';
 
 class SignIn extends StatefulWidget {
   const SignIn({super.key});
@@ -21,12 +17,14 @@ class _SignInState extends State<SignIn> {
   final TextEditingController _emailController = TextEditingController();
   final TextEditingController _passwordController = TextEditingController();
   bool _passwordState = false;
-  bool _signInState = false;
 
   @override
   void dispose() {
     _emailController.dispose();
     _passwordController.dispose();
+    if (userLocalSettings!.get("first_time")) {
+      userLocalSettings!.put("first_time", false);
+    }
     super.dispose();
   }
 
@@ -38,9 +36,22 @@ class _SignInState extends State<SignIn> {
         onTap: () => FocusScope.of(context).unfocus(),
         child: Stack(
           children: <Widget>[
+            CarouselSlider.builder(
+              itemCount: 4,
+              options: CarouselOptions(
+                viewportFraction: 1,
+                height: MediaQuery.sizeOf(context).height * .6,
+                autoPlay: true,
+                autoPlayCurve: Curves.easeIn,
+                enlargeFactor: 0,
+              ),
+              itemBuilder: (BuildContext context, int itemIndex, int pageViewIndex) {
+                return Image.asset("assets/pictures/football/${itemIndex + 1}.jpg", fit: BoxFit.cover);
+              },
+            ),
             Column(
               children: <Widget>[
-                SizedBox(height: MediaQuery.sizeOf(context).height * .35),
+                SizedBox(height: MediaQuery.sizeOf(context).height * .4),
                 Expanded(
                   child: Container(
                     decoration: const BoxDecoration(color: gray, borderRadius: BorderRadius.only(topLeft: Radius.circular(25), topRight: Radius.circular(25))),
@@ -50,18 +61,12 @@ class _SignInState extends State<SignIn> {
                       children: <Widget>[
                         Row(
                           children: <Widget>[
-                            const Text("Welcome\nBack", style: TextStyle(fontSize: 28, color: white, fontWeight: FontWeight.w500)),
+                            Text(userLocalSettings!.get("first_time") ? "Welcome" : "Welcome\nBack", style: const TextStyle(fontSize: 28, color: white, fontWeight: FontWeight.w500)),
                             const Spacer(),
                             Container(
                               decoration: BoxDecoration(color: white.withOpacity(.2), shape: BoxShape.circle),
                               padding: const EdgeInsets.all(16),
-                              child: const Icon(FontAwesome.fingerprint, color: teal, size: 25),
-                            ),
-                            const SizedBox(width: 10),
-                            Container(
-                              decoration: BoxDecoration(color: white.withOpacity(.2), shape: BoxShape.circle),
-                              padding: const EdgeInsets.all(16),
-                              child: const Icon(FontAwesome.hands, color: yellow, size: 25),
+                              child: Icon(FontAwesome.hands, color: yellow, size: 25),
                             ),
                           ],
                         ),
@@ -116,52 +121,29 @@ class _SignInState extends State<SignIn> {
                           ),
                         ),
                         const SizedBox(height: 40),
-                        StatefulBuilder(
-                          builder: (BuildContext context, void Function(void Function()) _) {
-                            return InkWell(
-                              highlightColor: transparent,
-                              splashColor: transparent,
-                              hoverColor: transparent,
-                              onTap: () async {
-                                try {
-                                  if (!_signInState) {
-                                    _(() => _signInState = false);
-                                    await FirebaseAuth.instance.signInWithEmailAndPassword(email: _emailController.text.trim(), password: _passwordController.text.trim());
-                                    await FirebaseFirestore.instance.collection("users").doc(FirebaseAuth.instance.currentUser!.uid).get().then((DocumentSnapshot<Map<String, dynamic>> value) async {
-                                      user = UserModel.fromJson(value.data()!);
-                                      await Navigator.pushReplacement(context, MaterialPageRoute(builder: (BuildContext context) => const Home()));
-                                      // ignore: use_build_context_synchronously
-                                      showSnack("User Authenitificated", 1, context);
-                                    });
-                                  }
-                                } catch (e) {
-                                  _(() => _signInState = false);
-                                  // ignore: use_build_context_synchronously
-                                  showSnack(e.toString(), 3, context);
-                                }
-                              },
-                              child: AnimatedContainer(
-                                duration: 700.ms,
-                                decoration: BoxDecoration(color: teal, borderRadius: BorderRadius.circular(12)),
-                                width: MediaQuery.sizeOf(context).width,
-                                padding: const EdgeInsets.all(20),
-                                child: Center(child: Text(_signInState ? "Waiting..." : "Sign in", style: const TextStyle(fontSize: 18, fontWeight: FontWeight.w500))),
-                              ),
-                            );
+                        InkWell(
+                          onTap: () async {
+                            await Get.to(const Home());
                           },
+                          child: Container(
+                            decoration: BoxDecoration(color: teal, borderRadius: BorderRadius.circular(12)),
+                            width: MediaQuery.sizeOf(context).width,
+                            padding: const EdgeInsets.all(20),
+                            child: const Center(child: Text("Sign in", style: TextStyle(fontSize: 18, fontWeight: FontWeight.w500))),
+                          ),
                         ),
                         const SizedBox(height: 20),
                         InkWell(
                           highlightColor: transparent,
                           splashColor: transparent,
                           hoverColor: transparent,
-                          onTap: () async => Navigator.pushReplacement(context, MaterialPageRoute(builder: (BuildContext context) => const SignUp())),
+                          onTap: () async => Get.to(const SignUp()),
                           child: Row(
                             mainAxisAlignment: MainAxisAlignment.center,
                             children: <Widget>[
                               Text("Don't have an account?", style: TextStyle(color: white.withOpacity(.5), fontSize: 16, fontWeight: FontWeight.w400)),
                               const SizedBox(width: 5),
-                              const Text("Get Started", style: TextStyle(color: teal, fontSize: 23, fontWeight: FontWeight.w400)),
+                              const Text("Get Started", style: TextStyle(color: teal, fontSize: 16, fontWeight: FontWeight.w400)),
                             ],
                           ),
                         ),
