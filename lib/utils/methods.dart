6a9b507a2a638b1711112e9c9c4164c103e584c8
Hafedh Gunichi <120606876+hafedh049@users.noreply.github.com>
2023-12-09 00:28:02 +0100

    ModifiedM lib/utils/methods.dart

--- lib/utils/methods.dart
+++ lib/utils/methods.dart
@@ -26,16 +26,17 @@ Future<bool> load() async {
         projectId: "harmonix-ede29",
       ),
     );
-    if({await loadUserLocalSettings())
-    if (userLocalSettings!.get("first_time") == null) {
-      await userLocalSettings!.put("first_time", true);
+    if (await loadUserLocalSettings()) {
+      if (userLocalSettings!.get("first_time") == null) {
+        await userLocalSettings!.put("first_time", true);
+      }
+      if (userLocalSettings!.get("theme") == null) {
+        await userLocalSettings!.put("theme", "dark");
+      }
+      if (userLocalSettings!.get("language") == null) {
+        await userLocalSettings!.put("language", "en");
+      }
     }
-    if (userLocalSettings!.get("theme") == null) {
-      await userLocalSettings!.put("theme", "dark");
-    }
-    if (userLocalSettings!.get("language") == null) {
-      await userLocalSettings!.put("language", "en");
-    }}
 
     return true;
   } catch (e) {
