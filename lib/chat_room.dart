663d557f20de92c102df6f2be51aaf3dad35629e
Hafedh Gunichi <120606876+hafedh049@users.noreply.github.com>
2023-12-08 22:20:31 +0100

    Modified  lib/chat_room.dart

--- lib/chat_room.dart
+++ lib/chat_room.dart
@@ -177,19 +177,7 @@ class _ChatRoomState extends State<ChatRoom> {
     }
 
     await FirebaseFirestore.instance.collection("trucks").doc(FirebaseAuth.instance.currentUser!.uid).get().then((DocumentSnapshot<Map<String, dynamic>> value) {
-      if (value.exists) {
-        <String, dynamic>{
-          'id': id,
-          'message': msg,
-          'createdAt': Timestamp.now(),
-          'sendBy': "1",
-          'message_type': messageType == MessageType.text
-              ? "text"
-              : messageType == MessageType.image
-                  ? "image"
-                  : "voice",
-        };
-      }
+      if (value.exists) {}
     });
 
     _chatController.setTypingIndicator = false;
