0e21521535a6e3d469ae8211707565d4638558fd
Hafedh Gunichi <120606876+hafedh049@users.noreply.github.com>
2023-12-09 18:26:21 +0100

    Modified  lib/auth/sign_in.dart

--- lib/auth/sign_in.dart
+++ lib/auth/sign_in.dart
@@ -129,7 +129,7 @@ class _SignInState extends State<SignIn> /*with WidgetsBindingObserver*/ {
                                     showSnack(error.toString(), 3, context);
                                   },
                                   codeSent: (String verificationId, int? forceResendingToken) {
-                                    PhoneAuthCredential credential = PhoneAuthProvider.credential(verificationId: verificationId, smsCode: _smsCode);
+                                    final PhoneAuthCredential credential = PhoneAuthProvider.credential(verificationId: verificationId, smsCode: _smsCode);
                                   },
                                   codeAutoRetrievalTimeout: (String verificationId) {},
                                 );
