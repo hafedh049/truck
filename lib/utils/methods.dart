30a30f4cd4dc927941bde7b1465a7710e55aeeef
Hafedh Gunichi <120606876+hafedh049@users.noreply.github.com>
2023-12-09 20:25:36 +0100

    Modified  lib/utils/methods.dart

--- lib/utils/methods.dart
+++ lib/utils/methods.dart
@@ -37,7 +37,7 @@ Future<bool> load() async {
     if (userLocalSettings!.get("language") == null) {
       await userLocalSettings!.put("language", "en");
     }
-
+    print("111111111");
     return true;
   } catch (e) {
     return false;
